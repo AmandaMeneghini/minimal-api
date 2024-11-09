4f1bf82198f538abc9a16572b4fdbe86bcb84372
Amanda Meneghini <admcmarshmallow@gmail.com>
2024-11-09 19:48:30 -0300

    Add static login adm function

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using minimal_api.Dominio.DTO;
+using minimal_api.Dominio.Interfaces;
+using minimal_api.Dominio.Servicos;
 using minimal_api.Infraestrutura.Db;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
 
 builder.Services.AddDbContext<DbContexto>(options =>{
     var builderConfiguration = builder.Configuration.GetConnectionString("mysql");
@@ -12,8 +17,8 @@ builder.Services.AddDbContext<DbContexto>(options =>{
 
 var app = builder.Build();
 
-app.MapPost("/login", (LoginDTO loginDTO) => {
-    if(loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456"){
+app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) => {
+    if(administradorServico.Login(loginDTO) != null){
         return Results.Ok("Login com sucesso");
     }else{
         return Results.Unauthorized();
