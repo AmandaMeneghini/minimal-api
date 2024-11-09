a90c69b2956e1a2e11f3af582c80c26ca8a1f590
Amanda Meneghini <admcmarshmallow@gmail.com>
2024-11-09 19:19:06 -0300

    Add migration of Adm

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
 using minimal_api.Dominio.DTO;
+using minimal_api.Infraestrutura.Db;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Jesus Vive!");
+builder.Services.AddDbContext<DbContexto>(options =>{
+    var builderConfiguration = builder.Configuration.GetConnectionString("mysql");
+
+    options.UseMySql(builderConfiguration, ServerVersion.AutoDetect(builderConfiguration));
+} );
+
+var app = builder.Build();
 
 app.MapPost("/login", (LoginDTO loginDTO) => {
     if(loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456"){
